e2660e0b27b132735cd5ea6ddc514d523a20e77a
ElijahGoat326 <elijahlopez326@gmail.com>
2022-11-23 08:58:48 -0500

    Made Actor class

--- src/Actor.java
+++ src/Actor.java
@@ -1,2 +1,13 @@
 public class Actor {
+    String AmountOfMoneyMade;
+    String filmAmount;
+    String totalmoneymade;
+    String amountoffilms;
+
+    public Actor(String amountOfMoneyMade, String filmAmount, String totalmoneymade, String amountoffilms) {
+        AmountOfMoneyMade = amountOfMoneyMade;
+        this.filmAmount = filmAmount;
+        this.totalmoneymade = totalmoneymade;
+        this.amountoffilms = amountoffilms;
+    }
 }
