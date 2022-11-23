c74d7c884d5f95c81fa4b625d301b23b7be5f93c
ElijahGoat326 <elijahlopez326@gmail.com>
2022-11-23 08:49:11 -0500

    Made Superclass

---
+++ src/Moviemaker.java
@@ -0,0 +1,14 @@
+public class Moviemaker {
+
+    String rank;
+    String name;
+    String average;
+    String numberOfFilms;
+
+    public Moviemaker(String rank, String name, String average, String numberOfFilms) {
+        this.rank = rank;
+        this.name = name;
+        this.average = average;
+        this.numberOfFilms = numberOfFilms;
+    }
+}
