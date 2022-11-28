26e292b14d99ba967d89c691192d5e33b178b5d3
ElijahGoat326 <elijahlopez326@gmail.com>
2022-11-28 14:42:25 -0500

    Added object to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Actor stanL = new Actor("1","Stan Lee","$624,606,668","49","$30,605,726,750","($2,797,732,053)","$30,605,726,750");
     }
 }
