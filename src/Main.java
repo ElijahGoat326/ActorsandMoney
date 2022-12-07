3fd003596676d64b640b6b8dce934956d206fc2b
ElijahGoat326 <elijahlopez326@gmail.com>
2022-12-07 09:19:09 -0500

    Finished describe self for actor

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Moviemaker> allmoviemakers = new ArrayList<>();
-        Actor stanL = new Actor("1","Stan Lee"," $624,606,668","49 films","$30,605,726,750","$2,797,732,053","$30,605,726,750");
+        Actor stanL = new Actor("1","Stan Lee"," $624,606,668","49 films"," $30,605,726,750"," $2,797,732,053");
         Directors stevenS = new Directors (" 1", " Steven Spielberg", " $287,567,687", " 37", " $10,640,004,408");
         allmoviemakers.add(stanL);
         allmoviemakers.add(stevenS);
