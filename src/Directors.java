1bbe392b086b52ee7768e3707ad271c7463bc080
zfriedman24 <zfriedman24@scienceleadership.org>
2022-12-08 09:59:51 -0500

    Describeself Corrections

--- src/Directors.java
+++ src/Directors.java
@@ -19,7 +19,7 @@ public class Directors extends Moviemaker {
     }
     // Methods
     void describeSelf() {
-        System.out.println("Hi!  I'm " + name + ", I'm a Director who's ranked #" + rank + " in the world. I'm most famous for directing " + HighestGrossingFilm + ", which was the most money any of my films made, but I've directed " + numberOfFilms + " over the span of my career, and in those films, I average " + average + "per film. In total, I've made " + worldwideTotal + " in revenue throughout my career.");
+        System.out.println("Hi! I'm" + name + ", I'm a Director who's ranked #" + rank + " in the world. I'm most famous for directing " + HighestGrossingFilm + ", which was the most money any of my films made, but I've directed" + numberOfFilms + " over the span of my career, and in those films, I average" + average + " per film. In total, I've made" + worldwideTotal + " in revenue throughout my career.");
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         new Actor("1","Stan Lee"," $624,606,668","49 films"," $30,605,726,750"," $2,797,732,053");
-        new Directors (" 1", " Steven Spielberg", " $287,567,687", " 37", " $10,640,004,408", "Jurassic Park $1,045,573,035");
+        new Directors (" 1", " Steven Spielberg", " $287,567,687", " 37", " $10,640,004,408", "Jurassic Park ($1,045,573,035)");
         for (Moviemaker maker: Moviemaker.getAllmoviemakers()) {
             maker.describeSelf();
         }
