3454bf64aa25cf7467f8a4b535b3ec96452256c1
zfriedman24 <zfriedman24@scienceleadership.org>
2022-12-08 10:17:21 -0500

    switch to tostring

--- src/Directors.java
+++ src/Directors.java
@@ -18,8 +18,9 @@ public class Directors extends Moviemaker {
         HighestGrossingFilm = highestGrossingFilm;
     }
     // Methods
-    void describeSelf() {
-        System.out.println("Hi! I'm" + name + ", I'm a Director who's ranked #" + rank + " in the world. I'm most famous for directing " + HighestGrossingFilm + ", which was the most money any of my films made, but I've directed" + numberOfFilms + " over the span of my career, and in those films, I average" + average + " per film. In total, I've made" + worldwideTotal + " in revenue throughout my career.");
+    public String toString() {
+        return ("Hi! I'm" + name + ", I'm a Director who's ranked #" + rank + " in the world. I'm most famous for directing " + HighestGrossingFilm + ", which was the most money any of my films made, but I've directed" + numberOfFilms + " over the span of my career, and in those films, I average" + average + " per film. In total, I've made" + worldwideTotal + " in revenue throughout my career.");
+
     }
 }
 
